92d4d3b6c7d32e8b7e371ad21cbb023522f22685
Mehul Kumawat <mehulkumawat2521@gmail.com>
2024-12-15 22:37:48 +0530

    Add files via upload

---
+++ 4.sql
@@ -0,0 +1,30 @@
+use employees;
+show tables;
+select * from employees;
+
+select * from payment
+where
+amount in(select amount 
+from payment where payment_id in (1,3));
+
+select * from payment
+where
+amount >any (select amount 
+from payment where payment_id in (1,3));
+
+
+select * from payment
+where
+amount <any (select amount 
+from payment where payment_id in (1,3));
+
+
+select * from payment
+where
+amount >all (select amount 
+from payment where payment_id in (1,3));
+
+select * from payment
+where
+amount <all (select amount 
+from payment where payment_id in (1,3));
